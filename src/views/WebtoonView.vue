9b944ecf79837c3d8b7c1cf74afa8cb28b33b4f2
magnae2016 <magnae2016@gmail.com>
2022-09-12 22:26:13 +0900

    Add Aside

--- src/views/WebtoonView.vue
+++ src/views/WebtoonView.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
-    <router-view></router-view>
+  <div id="container">
+    <div id="content" class="webtoon">
+      <router-view></router-view>
+    </div>
+    <app-aside></app-aside>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppAside from "@/components/layout/AppAside/index.vue";
 
 export default defineComponent({
-  setup() {
-    return {};
-  },
+    setup() {
+        return {};
+    },
+    components: { AppAside }
 });
 </script>
 
