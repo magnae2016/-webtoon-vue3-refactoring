672dff37a656586f17ba6a5d1cec7500de82bd6f
magnae2016 <magnae2016@gmail.com>
2022-09-12 21:08:03 +0900

    Add AppHeader on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/weekday">weekday</router-link> |
-    <router-link to="/weekdayList">weekdayList</router-link> |
-    <router-link to="/genre">genre</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrap">
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppHeader from "@/components/layout/AppHeader.vue";
 
-nav {
-  padding: 30px;
-}
+export default defineComponent({
+  setup() {
+    return {};
+  },
+  components: { AppHeader },
+});
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
 </style>
